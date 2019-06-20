fb7782320e086f69d76d7ca92e0bdbb8867311b2
Rob Pickering <rob@pickering.org>
2019-06-20 22:15:54 +0100

    Cherrypick master updatea

---
+++ cherrypick.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git cherry-pick -x bc33bbfcfb23c49552d697c9765662403a042536
+git cherry-pick -x a63b2750e97a5b692a58a01b140b14824624b07e
+git cherry-pick -x d9d5728522b5cb30a66cfda869bfbef3d4e53b0d
+git cherry-pick -x b42b4dfd821d61bfe20bdfdafaae17d363f29e2d
