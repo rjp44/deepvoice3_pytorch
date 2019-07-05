9ca29123ee075dfcda64ca3b7cfd7baa7bb3155c
Rob Pickering <rob@pickering.org>
2019-07-05 22:01:23 +0100

    fix prefix import

--- modeldef.sh
+++ modeldef.sh
@@ -1,7 +1,7 @@
-MODELURL="https://www.dropbox.com/s/5ucl9remrwy5oeg/20180505_deepvoice3_checkpoint_step000640000.pth"; export MODELURL
-PRESETURL="https://www.dropbox.com/s/0ck82unm0bo0rxd/20180505_deepvoice3_ljspeech.json"
-MODELFILE=$(basename ${MODELURL}); export MODELFILE
-PRESETFILE=$(basename ${MODELURL}); export PRESETFILE
+export MODELURL="https://www.dropbox.com/s/5ucl9remrwy5oeg/20180505_deepvoice3_checkpoint_step000640000.pth"
+export PRESETURL="https://www.dropbox.com/s/0ck82unm0bo0rxd/20180505_deepvoice3_ljspeech.json"
+export MODELFILE=$(basename ${MODELURL})
+export PRESETFILE=$(basename ${PRESETURL})
 
 
 if [ ! -f ${PRESETFILE} ]; then
